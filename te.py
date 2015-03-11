43b40552a2509433e5ad5f0caa5306575a0bb61e
Steve <stepbaro@gmail.com>
2015-03-11 13:10:23 +0000

    v1

---
+++ te.py
@@ -0,0 +1,8 @@
+#Find out how much you earn on the toilet in work time!
+# coding=utf-8 
+
+currency = raw_input("Do you earn in £ or $")
+if currency == '£':
+	print "calculations will take place in £ Sterling"
+else:
+	print "calculations will take place in US$"
