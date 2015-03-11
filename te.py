eb467cdced3fc908741957fe1a35ae599739fb6f
Steve <stepbaro@gmail.com>
2015-03-11 17:09:28 +0000

    v2.1

--- te.py
+++ te.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 #Find out how much you earn on the toilet in work time!
 
-currency = raw_input("Do you earn in %s or $") % p
-p = unichr(163)
+currency = raw_input("Do you earn in $ or $") 
+def wage_calc():
+	salary = input("What is you annual salary? ")
+	days = input("How many days a week do you work? ")
+	hours = input("How many hours a day do you work? ")
+	days = float(days)
+	hours = float(hours)
+	salary = float(salary)
+
 
 if currency == '/£':
 	print "calculations will take place in %s Sterling" % p
 else:
 	print "calculations will take place in US$"
 
-	wage()
+wage_calc()
 
+wage = days * hours
+wage2 = wage * 52
+
+print wage
 #calculate earnings
-def wage_calc():
-	salary = raw_input("How much do you earn? %s") % currency
-	days = raw_input("How many days a week do you work? ")
-	hours = raw_input("How many hours a day do you work? ")
 	
 
